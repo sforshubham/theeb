4b79921ee7ddfce3ebcd818be04587766a7eeebc
Tannu <tanujpant13@gmail.com>
2018-06-13 17:42:26 +0530

    FACTORY (CD-84): Add Agreement PDF Generation

--- resources/views/app/new_agreement.blade.php
+++ resources/views/app/new_agreement.blade.php
@@ -22,139 +22,31 @@
                                 @endif
                                 @php($row_count = count($result->Agreements->Agreement))
                                 @for ($i = 0; $i < $row_count; $i++)
-                                @php ($agreement = $result->Agreements->Agreement[$i])
-                            <div class="address-table border-all padding-all-10" style="margin-bottom:0">
-                                <div class="floatRight mg-rt-30 theeb-logo"><img src="{{url('/images/logo.png')}}" alt="شركة ذيب لتأجير السيارات" title="شركة ذيب لتأجير السيارات"></div>
-                                <div class="floatRight pd-top-15"><strong>Theeb Rent A Car Co</strong>
-                                    <br>{{ $result->Agreements->H1 }}
-                                    <br/>{{ $result->Agreements->H2 }}
-                                </div>
-                                <div class="clearBoth"></div>
-                            </div>
-                            <table cellpadding="0" cellspacing="0" width="100%" class="table-rental table-invoice">
-                                <tr>
-                                    <th>{{ __('Agreement No') }}</th>
-                                    <th colspan="3">{{ $agreement->AgreementNo }}</th>
-                                    <th colspan="2">{{ __('Branch') }}: {{ $agreement->CheckInBranch }}</th>
-                                    <th colspan="2">{{ __('Contact No') }}: {{ $agreement->DriverContactNo }}</th>
-                                </tr>
-                                <tr>
-                                    <td>{{ __('Driver ID') }}</td>
-                                    <td>{{ $agreement->DriverId }}</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td>{{ __('Agreement days') }}</td>
-                                    <td>{{ $agreement->AgreementDays }}</td>
-                                    <td>{{ __('Date') }}</td>
-                                    <td>{{ $agreement->CheckInDate }}</td>
-                                </tr>
-                                <tr>
-                                    <td>{{ __('Driver Name') }}</td>
-                                    <td>{{ $agreement->DriverName }}</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td>{{ __('Rental Package') }}</td>
-                                    <td>{{ $agreement->AgreementPackage}}</td>
-                                    <td>{{ __('Time') }}</td>
-                                    <td>{{ $agreement->CheckInDate }}</td>
-                                </tr>
-                                <tr>
-                                    <td>{{ __('License No') }}</td>
-                                    <td>{{ $agreement->DriverLicenseNo }}</td>
-                                    <td>{{ __('Expiry') }}</td>
-                                    <td>{{ $agreement->DriverLicenseExpiryDate }}</td>
-                                    <td>{{ __('Package Price') }}</td>
-                                    <td>{{ number_format($agreement->AgreementPackagePrice,2) }}</td>
-                                    <td>{{ __('Extras') }}</td>
-                                    <td>{{ $agreement->AgreementExtras }}</td>
-                                </tr>
-                                <tr>
-                                    <td>{{ __('Nationality') }}</td>
-                                    <td>{{ $agreement->DriverNationality }}</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td>{{ __('Charge Group') }}</td>
-                                    <td>{{ $agreement->AgreementChargeGroup }}</td>
-                                    <td>{{ __('Discount') }}</td>
-                                    <td>{{ $agreement->AgreementDiscount }}</td>
-                                </tr>
-                                <tr>
-                                    <td>{{ __('Membership No') }}</td>
-                                    <td>{{ $agreement->DriverMembershipNo }}</td>
-                                    <td>{{ __('Type') }}</td>
-                                    <td>{{ $agreement->DriverMembershipType }}</td>
-                                    <td>{{ __('Free KM') }}</td>
-                                    <td>{{ $agreement->AgreementFreeKm }}</td>
-                                    <td>{{ __('Free Hr') }}</td>
-                                    <td>{{ $agreement->AgreementFreeHr }}</td>
-                                </tr>
-                                <tr>
-                                    <td>{{ __('Mobile No') }}</td>
-                                    <td>{{ $agreement->DriverContactNo }}</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td>{{ __('Insurance (CDW)') }}</td>
-                                    <td>{{ $agreement->AgreementInsurance }}</td>
-                                    <td>{{ __('KM Out') }}</td>
-                                    <td>{{ $agreement->AgreementOutKm }}</td>
-                                </tr>
-                                <tr>
-                                    <td>{{ __('Work Tel') }}</td>
-                                    <td></td>
-                                    <td>{{ __('Home Tel') }}</td>
-                                    <td></td>
-                                    <td>{{ __('Extra Hr Charge') }}</td>
-                                    <td>{{ $agreement->AgreementExtraHourCharge }}</td>
-                                    <td>{{ __('Extra KM Charge') }}</td>
-                                    <td>{{ $agreement->AgreementExtraKmCharge }}</td>
-                                </tr>
-                                <tr>
-                                    <td>{{ __('Debitor No') }}</td>
-                                    <td>{{ $agreement->DebitorCode }}</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td>{{ __('Vehicle No') }}</td>
-                                    <td>{{ $agreement->LicenseNo }}</td>
-                                    <td>{{ __('Model') }}</td>
-                                    <td>{{ $agreement->AgreementModelName }}</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="2">{{ __('Debitor Name') }}</td>
-                                    <td colspan="2">{{ $agreement->DebitorName }}</td>
-                                    <td colspan="2">{{ __('Total Rental Amount') }}</td>
-                                    <td colspan="2">{{ number_format($agreement->AgreementTotalRental,2) }}</td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td>{{ __('Created User') }}</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                            </table>
-                            @endfor
+                                    @php ($agreement = $result->Agreements->Agreement[$i])
+
+                                    <div class="address-table padding-all-10 floatLeft">
+                                        
+                                        <span class="download_link" data-agreementno="{{ $i }}" style="color: #1269a0; cursor: pointer;">Download PDF</span>
+                                        <form action="{{ url('/download_rental_history_pdf') }}" method="post" name="download_pdf_{{ $i }}">
+                                            <input type="hidden" name="for" value="A">
+                                            <input type="hidden" name="agreement" value="{{ json_encode($agreement) }}">
+                                            <input type="hidden" name="h1" value="{{ $result->Agreements->H1 }}">
+                                            <input type="hidden" name="h2" value="{{ $result->Agreements->H2 }}">
+                                        </form>
+                                        <div class="clearBoth"></div>
+                                    </div>
+
+                                    @include('app.single_agreement', ['agreement' => $agreement, 'h1' => $result->Agreements->H1, 'h2' => $result->Agreements->H2])
+                                @endfor
                             <div class="clearBoth"></div>
-                        @else
-                        <div style="
-                            color:  grey;
-                            border-top: 1px grey solid;
-                            padding-top: 40px;
-                            margin-top: 20px;
-                        ">{{ __('Record not found') }}</div>
-                    @endif
+                            @else
+                                <div style="
+                                    color:  grey;
+                                    border-top: 1px grey solid;
+                                    padding-top: 40px;
+                                    margin-top: 20px;
+                                ">{{ __('Record not found') }}</div>
+                            @endif
                         </div>
 
                         <div class="clearBoth"></div>
@@ -165,3 +57,13 @@
 
 @stop
 @include('app.daterangepicker')
+@section('custom_script_new')
+<script type="text/javascript">
+    jQuery(function() {
+        jQuery('.download_link').click(function() {
+            var agreementno = jQuery(this).data('agreementno');
+            jQuery('form[name="download_pdf_' + agreementno + '"]').submit();
+        });
+    });
+</script>
+@stop
